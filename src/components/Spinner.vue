1c4326932d823f5452a91caaefc73d6b7988198f
Thangam-Arulseeli <seeliseelan@gmail.com>
2023-06-27 16:06:38 +0530

    First Commit

---
+++ src/components/Spinner.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="../assets/loading-spinner-image.jpg" />
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default{
+        name: 'SpinnerVue'
+    }
+</script>
+
+
